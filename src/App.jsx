0fbaafc4ef43ecab88cf34e188dc399ef045cd94
taraswww777 <taraswww777@mail.ru>
2024-09-08 14:43:55 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-600">
+      Hello world!
+    </h1>
+  )
+}
